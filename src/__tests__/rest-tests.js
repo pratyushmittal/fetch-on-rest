1ed3d22fd31f291d80b9717bc6fddfcd71dd9976
Pratyush Mittal <pratyushmittal@gmail.com>
2016-02-05 17:02:59 +0530

    Added more tests

--- src/__tests__/rest-tests.js
+++ src/__tests__/rest-tests.js
@@ -68,7 +68,10 @@ describe('test dependent libraries', function(){
     var Rest = require.requireActual('../rest.js');
     var api = new Rest('/api');
     expect(api._getUrl(['users', 'me'])).toEqual('/api/users/me');
-    expect(api._getUrl(['users', 'me'], {foo: 'bar'})).toEqual('/api/users/me?foo=bar');
+    // Example from https://blogs.dropbox.com/developers/2015/03/json-in-urls/
+    var params = JSON.stringify({a: "b", c: 4});
+    expect(api._getUrl(['users', 'me'], params)).toEqual(
+      '/api/users/me?%7B%22a%22%3A%22b%22%2C%22c%22%3A4%7D');
   });
 
   it('checks expansions with default get params', function() {
